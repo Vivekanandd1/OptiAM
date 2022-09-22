0ed5be4b3743fb32e597b19ab4cb0c0489cb0f5d
vivekanandd1 <vivekanandd1@live.com>
2022-09-23 03:25:54 +0530

    Generic Data Readability

--- src/test/java/OptiAM_Login/FileReader.java
+++ src/test/java/OptiAM_Login/FileReader.java
@@ -2,27 +2,40 @@ package OptiAM_Login;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class FileReader {
+	 
+	public String Url;
+	public String UserName;
+	public String Password;
+	 public String CaseNumber;
+	 
 	
+	  
+	public static void main(String[] args) throws IOException {
+		
 	
-	public static void FileReader() throws IOException {
-		File src  = new File("C:\\VivekDD\\OptiAM\\Info_Data.xlsx");
+		File src  = new File("C:\\Users\\Sony\\eclipse-workspace\\Optiam\\OptiAM\\Info_Data.xlsx");
 		FileInputStream fis = new FileInputStream(src);
 		XSSFWorkbook xsf = new XSSFWorkbook(fis);
 	   XSSFSheet sheet = xsf.getSheetAt(0);
-	   String Url = sheet.getRow(1).getCell(1).getStringCellValue();
-	   String UserName = sheet.getRow(2).getCell(1).getStringCellValue();
-	   String Password = sheet.getRow(3).getCell(1).getStringCellValue();
-	   String CaseNumber = sheet.getRow(3).getCell(1).getStringCellValue();
-	   System.out.println(CaseNumber);
+	String Url = sheet.getRow(1).getCell(1).getStringCellValue();
+	String UserName = sheet.getRow(2).getCell(1).getStringCellValue();
+	String Password = sheet.getRow(3).getCell(1).getStringCellValue();
+	String CaseNumber = sheet.getRow(4).getCell(1).getStringCellValue();
+
 	   xsf.close();
+	   System.out.println(Url);
+	   System.out.println(UserName);
+	   System.out.println(Password);
+	   System.out.println(CaseNumber);
+
 	}
-	
 		
 	}
 
--- src/test/java/OptiAM_Login/Login_Page.java
+++ src/test/java/OptiAM_Login/Login_Page.java
@@ -1,7 +1,12 @@
 package OptiAM_Login;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,16 +18,34 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.google.gson.annotations.Until;
 
-public class Login_Page extends FileReader {
+public class Login_Page  {
 	
-  
 	
-	public static void main(String[] args)  throws InterruptedException {
-		
+	public String Url;
+	public String UserName;
+	public String Password;
+	 public String CaseNumber;
+	 
+	
+	  
+	public  Login_Page() throws IOException {
+		File src  = new File("C:\\Users\\Sony\\eclipse-workspace\\Optiam\\OptiAM\\Info_Data.xlsx");
+		FileInputStream fis = new FileInputStream(src);
+		XSSFWorkbook xsf = new XSSFWorkbook(fis);
+	   XSSFSheet sheet = xsf.getSheetAt(0);
+	  this.Url = sheet.getRow(1).getCell(1).getStringCellValue();
+	  this.UserName = sheet.getRow(2).getCell(1).getStringCellValue();
+	   this.Password = sheet.getRow(3).getCell(1).getStringCellValue();
+	   this.CaseNumber = sheet.getRow(4).getCell(1).getStringCellValue();
+       xsf.close();
+
+	}
+	
+
+	public static void main(String[] args)  throws InterruptedException, IOException {
+		Login_Page Lp = new Login_Page();
 		By Login_ID = By.name("user_id");
-		String User_Name = "vivekanand";
 		By PasswordField = By.name("password");
-		String UserPassword = "Vivekanand@11888";
 		By Login_Btn = By.name("loginSubmit");
 		By LoanProgramSelection = By.xpath("//a[@title='Loan Program']");
 		By Usda_Btn = By.id("OptiAM USDA");
@@ -31,7 +54,6 @@ public class Login_Page extends FileReader {
 		By Servicing = By.xpath("(//li[@class='nav-item dropdown'])[1]");
 		By CaseSearch = By.xpath("(//a[contains(text(),'Case Search')])[1]");
 		By CaseNumberBox = By.xpath("//input[@title='LSO_USDA_FHACaseNo341234_title']");
-		String CaseNumber = "99900084444";  //
 		By SearchBtn = By.xpath("(//button[@ng-click='Search()'])[2]");
 		
 		
@@ -39,9 +61,9 @@ public class Login_Page extends FileReader {
 		
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get(Url);
-		driver.findElement(Login_ID).sendKeys(User_Name);
-		driver.findElement(PasswordField).sendKeys(UserPassword);
+		driver.get(Lp.Url);
+		driver.findElement(Login_ID).sendKeys(Lp.UserName);
+		driver.findElement(PasswordField).sendKeys(Lp.Password);
 		driver.findElement(Login_Btn).click();
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 		wait.until(ExpectedConditions.elementToBeClickable(LoanProgramSelection));
@@ -52,7 +74,7 @@ public class Login_Page extends FileReader {
 		driver.findElement(Servicing).click();
 		driver.findElement(CaseSearch).click();
 		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(CaseNumberBox));
-		driver.findElement(CaseNumberBox).sendKeys(CaseNumber);
+		driver.findElement(CaseNumberBox).sendKeys(Lp.CaseNumber);
 		driver.findElement(SearchBtn).click();
 		
 		Thread.sleep(10000);
