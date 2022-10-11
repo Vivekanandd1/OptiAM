7a82e3c4333d164c27a17fb28834643d731abb58
vivekanandd1 <vivekanandd1@live.com>
2022-10-11 23:45:34 +0530

    AutoIt initiation

--- src/test/java/OptiAM/AutoIt.java
+++ src/test/java/OptiAM/AutoIt.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,16 +12,28 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AutoIt {
-	public static void main(String[] args) throws IOException {	
+	public static void main(String[] args) throws IOException, InterruptedException {	
 		
 		
     WebDriver driver=new ChromeDriver();	
     
     driver.manage().window().maximize();
-    driver.get("https://cgi-lib.berkeley.edu/ex/fup.html");			
+    driver.get("https://formstone.it/components/upload/demo/");			
 	
+    JavascriptExecutor js = (JavascriptExecutor) driver;
+    js.executeScript("window.scrollBy(0,600)", "");
+    
+    Thread.sleep(3000);
+
+    driver.findElement(By.xpath("//*[@id=\"example-2\"]/form/div[1]/div")).click();
     // below line execute the AutoIT script .
-     Runtime.getRuntime().exec("C:\\Users\\QA\\Desktop\\Book2.xlsx");		
+    Thread.sleep(3000);
+
+    Runtime.getRuntime().exec("C:\\Users\\QA\\Desktop\\dump\\AutoitUploadScript.exe");
+    
+    Thread.sleep(3000);
+    
+    driver.findElement(By.xpath("//span[@class='start_all']")).click();
 
 //    driver.close();
      }
---
+++ src/test/java/OptiAM/NewMessaging.java
@@ -0,0 +1,19 @@
+package OptiAM;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class NewMessaging {
+	  private WebDriver driver;
+	  
+	  By More = By.xpath("//a[normalize-space()='More']");
+	
+	public NewMessaging(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public void NewDevalMessaging() {
+		
+	}
+
+}
